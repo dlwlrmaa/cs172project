cd0f563a4a9a9e47bf17407a988f475112f437ec
Jeffrey Juan <JeffJuan@Jeffreys-MacBook-Air.local>
2020-05-04 14:07:45 -0700

    first push

---
+++ collect_tweets.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import config
+import requests, json, os, re
+from tweepy import OAuthHandler, Stream
+from tweepy.streaming import StreamListener
+
+
+if __name__ == '__main__':
+    pass

8000b1a069cc9eaf924f0a8ecc32822ba462b067
Jeffrey Juan <JeffJuan@Jeffreys-MacBook-Air.local>
2020-05-06 15:12:42 -0700

    parsed tweet data to only collect relevant info

--- collect_tweets.py
+++ collect_tweets.py
@@ -1,10 +1,77 @@
 #!/usr/bin/env python
 
 import config
-import requests, json, os, re
+import requests
+import json 
+import os
+import re
 from tweepy import OAuthHandler, Stream
 from tweepy.streaming import StreamListener
 
+output_file = 'output_tweets.json'
+api_key = config.API_key
+api_secret = config.API_secret
+access_token = config.access_token
+access_secret = config.access_secret
+
+class streamListener(StreamListener):
+
+    def on_data(self, data):
+        parsed_json = json.loads(data)
+        user = parsed_json["user"]["screen_name"]
+        urls  = parsed_json["entities"]["urls"]
+        #parsed_url = parsed_json["expanded_url"]
+        location = parsed_json["place"]["full_name"]
+        timestamp = parsed_json["created_at"]
+        truncated = parsed_json["truncated"]
+        if( not truncated):
+            text = parsed_json["text"]
+
+            dictionary ={
+                "user": str(user),
+                "text": str(text),
+                "url" : str(urls),
+                "location" : str(location),
+                "timestamp" : str(timestamp)
+            }
+            #tweet = {"USER:" + str(user) + " TEXT: "+ str(text) + " URL: " + str(urls) + " LOCATION: " + str(location) + " TIMESTAMP: " + str(timestamp) + '\n'}
+            numtweets = 1
+        
+        else:
+            extendedtext = parsed_json["extended_tweet"]["full_text"]
+
+            dictionary = {
+                "user": str(user),
+                "text": str(extendedtext),
+                "url" : str(urls),
+                "location" : str(location),
+                "timestamp" : str(timestamp)
+            }
+            tweet = {"USER:" + str(user) + " TEXT: "+ str(extendedtext) + " URL: " + str(urls) + " LOCATION: " + str(location) + " TIMESTAMP: " + str(timestamp) + '\n'}
+            numtweets = 2
+
+        with open(output_file, 'a+') as output:
+            json.dump(dictionary, output)
+            output.write('\n')
+            print(numtweets)
+            return True
+        return False
+
+
+    def on_error(self, status):
+        print(status)
+#end of streamListener
 
 if __name__ == '__main__':
-    pass
+    new_stream_listener = streamListener()
+    auth = OAuthHandler(api_key, api_secret)
+    auth.set_access_token(access_token, access_secret)
+    myStream = Stream(auth, new_stream_listener)
+
+    if os.path.isfile(output_file):
+        os.remove(output_file)
+        f'File {output_file} has been reinitialized'
+
+    myStream.filter(locations = [-118.69, 33.73, -117.85, 34.22]) #coordinates are for Los Angeles
+
+#resource http://docs.tweepy.org/en/latest/streaming_how_to.html

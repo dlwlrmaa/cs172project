f267aef43917c920852f51551c5e1df65929d1df
Eric <echai009@ucr.edu>
2020-05-08 21:15:06 -0700

    Uncommented driver code to collect tweets

--- collect_tweets.py
+++ collect_tweets.py
@@ -59,7 +59,7 @@ class streamListener(StreamListener):
             print("Extended")
         #timedone = 0
         #Change tweetcount limit to what is acceptable
-        if(tweetcount >= 25):
+        if(tweetcount >= 1000):
             #timedone = time.process_time()
             #print(f"{timedone/60} minutes")
             print("Done collecting tweets!")
@@ -84,12 +84,11 @@ def URLTitleFinder(tweetFile):
 			data = json.loads(line)
 			pageURL = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', data["text"])
 			if data["url"] != [] and (expanded_url in data["url"][0]) and pageURL != []:
-				print(data["url"][0])
+				#print(data["url"][0])
 				pageURL = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', data['url'][0]['expanded_url'])
 				if pageURL!= []:
 					for i in range(len(pageURL)):
-						print("in loop")
-						print(pageURL[i])
+						#print(pageURL[i])
 						soup = BeautifulSoup(urllib.request.urlopen(pageURL[i]).read(), "html.parser")
 						title = soup.title.string.encode("utf-8")
 						data.update({"title" : str(title)})
@@ -98,7 +97,6 @@ def URLTitleFinder(tweetFile):
 
 if __name__ == '__main__':
     #numtweets = 0
-    """
     new_stream_listener = streamListener()
     auth = OAuthHandler(api_key, api_secret)
     auth.set_access_token(access_token, access_secret)
@@ -109,6 +107,5 @@ if __name__ == '__main__':
         print(f"File {output_file} has been reinitialized")
 
     myStream.filter(locations = [-118.69, 33.73, -117.85, 34.22]) #coordinates are for Los Angeles
-    """
     URLTitleFinder('output_tweets.json')
 #resource http://docs.tweepy.org/en/latest/streaming_how_to.html

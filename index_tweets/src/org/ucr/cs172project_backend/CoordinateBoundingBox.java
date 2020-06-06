39f88de7916c48eb36eaa56ee5258a014f418b9c
jchoi071 <jchoi071@ucr.edu>
2020-06-06 00:06:02 -0700

    Java backend

---
+++ index_tweets/src/org/ucr/cs172project_backend/CoordinateBoundingBox.java
@@ -0,0 +1,25 @@
+package org.ucr.cs172project_backend;
+
+public class CoordinateBoundingBox {
+    private double lat1;
+    private double long1;
+    private double lat2;
+    private double long2;
+
+    public CoordinateBoundingBox(double lt1, double lg1, double lt2, double lg2) {
+        lat1 = lt1;
+        long1 = lg1;
+        lat2 = lt2;
+        long2 = lg2;
+    }
+
+    public double[] Center()
+    {
+        double[] center = {0, 0};
+
+        center[0] = lat1 + lat2 / (double) 2;
+        center[1] = long1 + long2 / (double) 2;
+
+        return center;
+    }
+}
